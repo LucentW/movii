6e5476b2fca146c9a5075b9ab11c14291b282f87
Hamcha <zikyky@gmail.com>
2014-06-08 14:46:26 +0200

    Basic parsing getting less basic

--- src/Script.go
+++ src/Script.go
@@ -7,12 +7,13 @@ import (
 type ActionType int
 
 const (
-	ACTION_ENTER ActionType = iota
-	ACTION_LEFT             = iota
-	ACTION_SAY              = iota
-	ACTION_PAUSE            = iota
-	ACTION_EVENT            = iota
-	ACTION_NULL             = iota
+	ACTION_JOIN   ActionType = iota
+	ACTION_MASTER            = iota
+	ACTION_LEAVE             = iota
+	ACTION_SAY               = iota
+	ACTION_PAUSE             = iota
+	ACTION_EVENT             = iota
+	ACTION_NULL              = iota
 )
 
 type Action struct {
@@ -25,16 +26,41 @@ func ParseLine(line string) Action {
 	var out Action
 	line = strings.Trim(line, " \r\n")
 
+	// Empty line - SKIP
 	if len(line) < 1 {
 		out.Type = ACTION_NULL
 		return
 	}
 
+	// Character: Message - DIALOGUE
 	if sep := strings.Index(line, ":"); sep > 0 {
 		out.Type = ACTION_SAY
 		out.Who = strings.Trim(line[:sep], " ")
 		out.What = strings.Trim(line[sep+1:], " \r")
 		return out
+	}
+
+	// =Character - JOIN MASTER
+	if line[0] == '=' {
+		out.Type = ACTION_MASTER
+		out.Who = lines[1:]
+	}
+
+	// +Character - JOIN
+	if line[0] == '+' {
+		out.Type = ACTION_JOIN
+		out.Who = lines[1:]
+	}
+
+	// -Character - LEAVE
+	if line[0] == '-' {
+		out.Type = ACTION_LEAVE
+		out.Who = lines[1:]
+	}
+
+	// PAUSE
+	if line == "PAUSE" {
+		out.Type = ACTION_PAUSE
 	}
 }
 
